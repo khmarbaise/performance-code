14add2d1642862e668639c2ca217db6d77a198fb
Karl Heinz Marbaise <kama@soebes.de>
2024-07-28 13:38:45 +0200

    Removed unused import.

--- src/main/java/com/soebes/performance/strings/StringAppenderBenchmark.java
+++ src/main/java/com/soebes/performance/strings/StringAppenderBenchmark.java
@@ -12,7 +12,6 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
